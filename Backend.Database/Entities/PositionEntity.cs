61554f8169dd8cca1827ff101fe99ec04ee81b46
Vastag Atila <wasyster@gmail.com>
2022-12-20 14:17:35 +0100

    added Backend.Database code

---
+++ Backend.Database/Entities/PositionEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace Backend.Database.Entities;
+
+[Table("Position")]
+public class PositionEntity
+{
+    [Key]
+    [Required]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(255)]
+    public string Name { get; set; }
+
+    public virtual IList<PlayerEntity> Players { get; set; }
+}
